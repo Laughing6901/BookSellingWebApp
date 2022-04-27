3aa145f1a5e518fffd087c6ff58a9e083082d7a8
Nguyen Tam <nguyenvothanhtam7@gmail.com>
2022-04-27 22:54:21 +0700

    create form

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,17 +1,19 @@
-import { Suspense } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./components/views/Login";
+import Register from "./components/views/Register";
+import Forgot from "./components/views/Forgot";
 
-function App() {
+const Auth = () => {
   return (
-    <Suspense fallback="...loading">
-      <BrowserRouter>
-        <Switch>
-          
-        </Switch>
-      </BrowserRouter>
-    </Suspense>
+    <Router>
+      <Switch>
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
+        <Route path='/forgot-password' component={Forgot} />
+        <Route path='/' component={Login} />
+      </Switch>
+    </Router>
   );
 }
 
-export default App;
+export default Auth;
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from "./reportWebVitals";
-
-import 'antd/dist/antd.css'
-import { createStore, applyMiddleware, compose } from 'redux';
-import { Provider } from 'react-redux';
-import { rootReducer } from './reducers';
-import thunk from 'redux-thunk';
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-export const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
+import reportWebVitals from './reportWebVitals';
+import './assets/scss/auth.scss'
 
 ReactDOM.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
