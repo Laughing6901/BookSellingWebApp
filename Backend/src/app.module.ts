c229c0c1f98ef741aaef5cb347608fb2e98e64d6
Laughing6901 <vandat96tn@gmail.com>
2022-04-27 13:58:22 +0700

    generate chat and user folder

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
+import { ChatController } from './chat/chat.controller';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UserModule, ChatModule],
+  controllers: [AppController, UserController, ChatController],
   providers: [AppService],
 })
 export class AppModule {}
