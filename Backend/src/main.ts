bdfa9b169588623116b1c92e8af9b4c2bb2759bd
Laughing6901 <vandat96tn@gmail.com>
2022-04-28 20:17:49 +0700

    done config env for deploy

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get("PRODUCT_PORT") || 8000;
+  const port = configService.get("PRODUCT_PORT") || 5500;
   console.log(port);
   await app.listen(port);
 }
