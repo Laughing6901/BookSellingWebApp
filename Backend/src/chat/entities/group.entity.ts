82016385bcca9e8b84a2e481175dfb5e281d8ad9
Laughing6901 <vandat96tn@gmail.com>
2022-05-03 15:12:21 +0700

    create entity group chat

---
+++ Backend/src/chat/entities/group.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity("GroupChat")
+export class GroupChat {
+    @PrimaryColumn({
+        name: "GroupId",
+        type: "int"
+    })
+    GroupId: number;
+
+    @Column({
+        name: "GroupName",
+        type: "varchar"
+    })
+    GroupName: string;
+
+    @Column({
+        name: "Avatar",
+        type: 'blob'
+    })
+    Avatar: string;
+}
+
