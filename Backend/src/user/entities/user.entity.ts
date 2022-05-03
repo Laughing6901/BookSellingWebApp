76d5964b4c25b6273bc4ce180b749d03d7b3edda
Laughing6901 <vandat96tn@gmail.com>
2022-05-03 12:19:26 +0700

    add user entity

--- Backend/src/user/entities/user.entity.ts
+++ Backend/src/user/entities/user.entity.ts
@@ -1,1 +1,31 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('Users')
+export class User {
+    @PrimaryGeneratedColumn({name: "UserId", type: 'int'})
+    UserId: number;
+
+    @Column({name: "FirstName", type: 'varchar', length: 20})
+    FirstName: string;
+
+    @Column({name: "LastName", type: 'varchar', length: 20})
+    LastName: string;
+
+    @Column({name: "Password", type: 'varchar', length: 20})
+    Password: string;
+
+    @Column({name: "Email", type: "varchar"})
+    Email: string;
+
+    @Column({name: "Gender", type: "varchar", length: 6})
+    Gender: string;
+
+    @Column({name: "DateOfBirth" , type:"date" })
+    DateOfBirth: Date;
+
+    @Column({name: "Address", type: 'varchar'})
+    Address: string;
+
+    @Column({name: "PhoneNumber", type:'string'})
+    PhoneNumber: string;
+}
