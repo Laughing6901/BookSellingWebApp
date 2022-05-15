a2401fc473b2e2746ff29ca4a0fab0145b508568
Nguyen Tam <nguyenvothanhtam7@gmail.com>
2022-05-15 21:23:33 +0700

    first commit

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,13 +1,15 @@
 import { Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Login from "./components/views/Login";
 
 function App() {
   return (
     <Suspense fallback="...loading">
       <BrowserRouter>
         <Switch>
-          
+        <Route path='/login' component={Login} />
+        <Route path='/' component={Login} />
         </Switch>
       </BrowserRouter>
     </Suspense>
