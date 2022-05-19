93b49083bc440e4ebd82a7d6fca4b7f1c5a6a09d
Laughing6901 <vandat96tn@gmail.com>
2022-05-19 21:30:59 +0700

    done fix UI render theme

--- Frontend/src/ChatRoom/index.js
+++ Frontend/src/ChatRoom/index.js
@@ -22,6 +22,7 @@ export default function ChatRoom(props) {
   console.log(props.match)
   console.log(id)
   const [theme, setTheme] = useState(localStorage.getItem('themeOption'))
+  console.log("theme:",theme);
   const [itemActive, setItemActive] = useState();
 
   const body = document.body
@@ -34,6 +35,7 @@ export default function ChatRoom(props) {
   else {
     body.classList.add(lightTheme)
     localStorage.setItem('themeOption', 'light')
+    setTheme(lightTheme);
   }
 
   const onChangeTheme = (formState) => {
