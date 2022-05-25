d39f2c3f0176d0c62a042b3fe15f849116db431c
Nguyen Tam <nguyenvothanhtam7@gmail.com>
2022-05-25 10:32:28 +0700

    newform

--- Frontend/src/components/views/Login.js
+++ Frontend/src/components/views/Login.js
@@ -59,11 +59,6 @@ const Login = () => {
 
     return (
         <div className="row g-0 auth-wrapper">
-            <div className="col-12 col-md-5 col-lg-6 h-100 auth-background-col">
-                <div className="auth-background-holder"></div>
-                <div className="auth-background-mask"></div>
-            </div>
-
             <div className="col-12 col-md-7 col-lg-6 auth-main-col text-center">
                 <div className="d-flex flex-column align-content-end">
                     <div className="auth-body mx-auto">
