f743f338c565c9b2cd0309f1f15fdbd897a5dae6
sarah <sarahphan2314@gmail.com>
2022-05-27 20:39:24 +0700

    change input function

--- Frontend/src/Message/index.js
+++ Frontend/src/Message/index.js
@@ -1,4 +1,5 @@
-import { Input } from 'antd'
+import { SendOutlined } from '@ant-design/icons'
+import { Button, Form, Input, Row, Col } from 'antd'
 import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import BoxHeader from './BoxHeader'
@@ -7,10 +8,10 @@ import './style.css'
 
 export default function Message() {
     const dispatch = useDispatch();
-   
+
     const renderBoxHeader = () => {
-        return(
-            <BoxHeader/>
+        return (
+            <BoxHeader />
         )
     }
 
@@ -19,20 +20,42 @@ export default function Message() {
         dispatch(changeTrigger(trigger))
     }, [trigger, dispatch])
 
+    const receiveMessage = (values) => {
+        console.log(values)
+    }
+
     return (
         <div className='messageBox'>
             {renderBoxHeader()}
             <div className='messageList'></div>
             <div className='sendMessage'>
-                <form>
-                    <div className='inputGroup'>
-                        <div className='inputText'>
-                            <Input placeholder="Message" allowClear onClick={() => {
-                                isTrigger(!trigger)
-                            }}/>
-                        </div>
-                    </div>
-                </form>
+                <Form
+                    onFinish={receiveMessage}
+                >
+                    <Row className="inputMessage">
+                        <Col span={14}>
+                            <Form.Item
+                                name="messageInput"
+                            >
+                                <Input
+                                    placeholder="Message"
+                                    allowClear
+                                    onClick={() => {
+                                        isTrigger(!trigger)
+                                    }}
+                                />
+                            </Form.Item>
+                        </Col>
+                        <Col span={4}>
+                            <Form.Item>
+                                <Button htmlType='submit'>
+                                    <SendOutlined />
+                                </Button>
+                            </Form.Item>
+                        </Col>
+                    </Row>
+
+                </Form>
             </div>
         </div>
     )
