f1dd80011f689ecad902519ebcc9258c41877cfc
sarah <sarahphan2314@gmail.com>
2022-06-01 21:19:38 +0700

    config env

--- Backend/ormconfig.ts
+++ Backend/ormconfig.ts
@@ -7,8 +7,8 @@ const typeOrmConfig:ConnectionOptions = {
   type: 'mysql',
   host: process.env.HOST || "localhost",
   port: parseInt(process.env.DB_PORT) || 3306,
-  username: process.env.USERNAME || "root",
-  password: process.env.PASSWORD || "root",
+  username: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD || "root",
   database: process.env.DATABASE || "root",
   entities: ["dist/src/**/*.entity{.ts,.js}"],
   synchronize: false, // true is Unsafe not use for product and migration
