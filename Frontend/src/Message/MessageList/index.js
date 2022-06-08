08c96b1836542e1211c83d8c2d4b6ab87d5c12a5
sarah-phan <72535562+sarah-phan@users.noreply.github.com>
2022-06-08 20:46:30 +0700

    fix message UI

--- Frontend/src/Message/MessageList/index.js
+++ Frontend/src/Message/MessageList/index.js
@@ -1,28 +1,28 @@
 import React, { useEffect } from 'react'
-import {Row, Col, Avatar} from "antd"
+import { Row, Col, Avatar } from "antd"
 import { UserOutlined } from '@ant-design/icons'
 import { store } from '../../store'
 import { useSelector } from 'react-redux'
 
 export default function MessageList() {
     let messageChat = useSelector(state => state.chatMessage);
-    
+
     const distinguish = (msg) => {
         return msg.map((item, index) => {
-            if(item.socketId === messageChat.socketId){
-                return(
+            if (item.socketId === messageChat.socketId) {
+                return (
                     <div key={index} className='messageContainerAcc1 animate__animated animate__slideInUp animate_fast'>
                         <div className='messageAcc1'>{item.message}</div>
                     </div>
                 )
             }
             else {
-                return(
+                return (
                     <div key={index} className='messageContainerAcc2 animate__animated animate__slideInUp animate_fast'>
                         <div className='messageAcc2'>
-                            <Row wrap= {false} >
+                            <Row wrap={false} >
                                 <Col>
-                                    <Avatar size={40} icon={<UserOutlined style={{ fontSize: 20 }} />} className='messageAvatar'/>
+                                    <Avatar size={40} icon={<UserOutlined style={{ fontSize: 20 }} />} className='messageAvatar' />
                                 </Col>
                                 <Col className='messageDetail'>
                                     {item.message}
@@ -37,10 +37,12 @@ export default function MessageList() {
     useEffect(() => {
         console.log(messageChat);
     })
-  return (
-    <div className='messageList'>
-        {distinguish(messageChat.messages)}
-    </div>
+    return (
+        <div className='messageList'>
+            <div>
+                {distinguish(messageChat.messages)}
+            </div>
+        </div>
 
-  )
+    )
 }
