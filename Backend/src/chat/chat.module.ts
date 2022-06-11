f81e050b8bb514c6186382b7d290ac0d8ba9f4e6
Laughing6901 <vandat96tn@gmail.com>
2022-06-11 09:50:32 +0700

    update

--- Backend/src/chat/chat.module.ts
+++ Backend/src/chat/chat.module.ts
@@ -1,8 +1,15 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { ChatController } from "./chat.controller";
 import { ChatService } from "./chat.service";
+import { GroupChatRepository } from "./repositories/group.repository";
+import { GroupMemberRepository } from "./repositories/member-group.repository";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([
+    GroupChatRepository,
+    GroupMemberRepository
+  ])],
   controllers: [ChatController],
   providers: [ChatService],
 })
